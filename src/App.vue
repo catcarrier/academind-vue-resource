7ee9e8c692bb2aae6e619bce47bec162ce705146
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-08-08 16:57:09 -0500

    added interceptor and resource examples

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,12 @@
                     <input type="text" class="form-control" v-model="user.email">
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
+                <br><br>
+                <button class="btn btn-primary" @click="fetch">Fetch</button>
+                <br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users">{{ u.username }} | {{ u.email }}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -24,22 +30,60 @@ export default {
       user: {
         username: "",
         email: ""
-      }
+      },
+      users: [],
+      resource: {}
     };
   },
   methods: {
     submit: function() {
-      this.$http
-        .post("https://vuejs-http-6bd59.firebaseio.com/data.json", this.user)
-        .then(
-          response => {
-            console.log(response);
-          },
-          error => {
-            console.log(error);
-          }
-        );
+    //   this.$http
+    //     .post("data.json", this.user)
+    //     .then(
+    //       response => {
+    //         this.fetch();
+    //       },
+    //       error => {
+    //         console.log(error);
+    //       }
+    //     );
+        const params = {};
+        // this.resource.save(params, this.user);
+        this.resource.saveAlt(params, this.user);
+
+    },
+    fetch: function(){
+        this.$http
+            .get("data.json") // root path is defined in main.js
+            .then( 
+                response => {
+                    return response.json(); // helper method from vue-resource, returns Promise
+                }, 
+                error => { 
+                    console.log("the error is ", error)
+                })
+            .then( 
+                parsedData => {
+                    console.log(parsedData);
+                    const newUsers = [];
+                    var keys = Object.keys(parsedData);
+                    for(var i = 0;i<keys.length;i++){
+                        var key = keys[i];
+                        newUsers.push( parsedData[key] )
+                    }
+                    this.users = newUsers;
+            })
     }
+  },
+  created: function(){
+      const customActions = {
+          saveAlt: {
+              method:'POST',
+              url: 'alternative.json'
+          }
+      };
+      const varData = {};
+      this.resource = this.$resource('data.json', varData, customActions);
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource';
 import App from './App.vue'
+import { request } from 'http';
 
 Vue.use(VueResource);
+
+// All http request will go to this url; append to this path as needed
+Vue.http.options.root = "https://vuejs-http-6bd59.firebaseio.com/"
+
+Vue.http.interceptors.push( (request, next) => {
+  console.log(request);
+  
+  // Intercept the request:
+  // change method from POST to PUT
+  if(request.method == 'POST') { request.method = 'PUT'; }
+
+  // Intercept the response:
+  // Override vue-resource's response.json() method to return the body in a 'messages' key
+  next(response => {
+    response.json= () => { return { messages: response.body} }
+  });
+} );
+
+
 
 new Vue({
   el: '#app',
