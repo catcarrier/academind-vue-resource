77633ede3735dfc43b1873641fbaa77202fd67fb
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-08-08 12:30:39 -0500

    updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,45 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                    <label for="">Username</label>
+                    <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label for="">Mail</label>
+                    <input type="text" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
+export default {
+  data: function() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      }
+    };
+  },
+  methods: {
+    submit: function() {
+      this.$http
+        .post("https://vuejs-http-6bd59.firebaseio.com/data.json", this.user)
+        .then(
+          response => {
+            console.log(response);
+          },
+          error => {
+            console.log(error);
+          }
+        );
     }
+  }
+};
 </script>
 
 <style>
